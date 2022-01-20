3ce856ac7fd2b5e9f17e7da8739fb3552f1e2f1b
Zian Asti Dwiyanti <ziandwiasti23@gmail.com>
2022-01-20 22:27:15 +0700

    env and config modiffied

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DB,
-    host: process.env.MYSQL_HOST,
+    username: 'root',
+    password: '',
+    database: 'clinic',
+    host: 'localhost',
     dialect: process.env.DB_TYPE,
     operatorsAliases: 0,
   },
